c300e41d3d1b0258d829ec848033dc392701b301
Vinh Nguyen <kurei@axcoto.com>
2017-06-13 00:23:08 -0700

    Add joken

--- community/betterdev/mix.exs
+++ community/betterdev/mix.exs
@@ -35,6 +35,7 @@ defmodule Betterdev.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
+     {:joken, "~> 1.1"},
      {:cowboy, "~> 1.0"}]
   end
 
