f5a0abad6d07b95896e585f0ed5b8fb2eaa0be26
Vinh Nguyen <kurei@axcoto.com>
2017-06-18 13:48:28 -0700

    Auth0 on NPM

--- community/betterdev/assets/js/app.js
+++ community/betterdev/assets/js/app.js
@@ -18,4 +18,20 @@ import "phoenix_html"
 // Local files can be imported directly using relative
 // paths "./socket" or full ones "web/static/js/socket".
 
-// import socket from "./socket"
+// import socket from "./socket"<
+import Auth0Lock from 'auth0-lock'
+const lock = window.lock = new Auth0Lock('a-wb-XWRmIfAm9v0U9eUbfawoJKsGG99', 'yeo.auth0.com', {
+	auth: {
+		redirect: false,
+		//redirectUrl: 'http://127.0.0.1:4000',
+		//responseType: 'token',
+		responseType: 'id_token',
+		params: {
+			scope: 'openid email'
+		}
+	}
+})
+
+lock.on("authenticated", function(authResult) {
+	localStorage.setItem('accessToken', authResult.idToken);
+})
