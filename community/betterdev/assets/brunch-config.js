bb0664bdfb3d73da84554484256bb1416a9b6eaa
Vinh Nguyen <kurei@axcoto.com>
2017-06-17 19:43:28 -0700

    UI

--- community/betterdev/assets/brunch-config.js
+++ community/betterdev/assets/brunch-config.js
@@ -37,7 +37,7 @@ exports.config = {
   // Phoenix paths configuration
   paths: {
     // Dependencies and current project directories to watch
-    watched: ["static", "css", "js", "vendor"],
+    watched: ["static", "css", "js", "vendor", "elm/src"],
     // Where to compile files to
     public: "../priv/static"
   },
@@ -47,7 +47,19 @@ exports.config = {
     babel: {
       // Do not use ES6 compiler in vendor code
       ignore: [/vendor/]
-    }
+    },
+		plugins: {
+			elmBrunch: {
+        elmFolder: "elm",
+        mainModules: ["src/Main.elm"],
+        outputFolder: "../vendor",
+        makeParameters: ['--debug']
+			},
+      babel: {
+				// Do not use ES6 compiler in vendor code
+				ignore: [/web\/static\/vendor/]
+			},
+		},
   },
 
   modules: {
