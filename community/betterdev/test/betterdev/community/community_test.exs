dc7cb153b68520f782d2c8b2959395dce6788e89
Vinh Nguyen <kurei@axcoto.com>
2017-07-03 22:12:10 -0700

    Add test

--- community/betterdev/test/betterdev/community/community_test.exs
+++ community/betterdev/test/betterdev/community/community_test.exs
@@ -64,4 +64,66 @@ defmodule Betterdev.CommunityTest do
       assert %Ecto.Changeset{} = Community.change_link(link)
     end
   end
+
+  describe "collections" do
+    alias Betterdev.Community.Collection
+
+    @valid_attrs %{name: "some name", user_id: 42}
+    @update_attrs %{name: "some updated name", user_id: 43}
+    @invalid_attrs %{name: nil, user_id: nil}
+
+    def collection_fixture(attrs \\ %{}) do
+      {:ok, collection} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> Community.create_collection()
+
+      collection
+    end
+
+    test "list_collections/0 returns all collections" do
+      collection = collection_fixture()
+      assert Community.list_collections() == [collection]
+    end
+
+    test "get_collection!/1 returns the collection with given id" do
+      collection = collection_fixture()
+      assert Community.get_collection!(collection.id) == collection
+    end
+
+    test "create_collection/1 with valid data creates a collection" do
+      assert {:ok, %Collection{} = collection} = Community.create_collection(@valid_attrs)
+      assert collection.name == "some name"
+      assert collection.user_id == 42
+    end
+
+    test "create_collection/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Community.create_collection(@invalid_attrs)
+    end
+
+    test "update_collection/2 with valid data updates the collection" do
+      collection = collection_fixture()
+      assert {:ok, collection} = Community.update_collection(collection, @update_attrs)
+      assert %Collection{} = collection
+      assert collection.name == "some updated name"
+      assert collection.user_id == 43
+    end
+
+    test "update_collection/2 with invalid data returns error changeset" do
+      collection = collection_fixture()
+      assert {:error, %Ecto.Changeset{}} = Community.update_collection(collection, @invalid_attrs)
+      assert collection == Community.get_collection!(collection.id)
+    end
+
+    test "delete_collection/1 deletes the collection" do
+      collection = collection_fixture()
+      assert {:ok, %Collection{}} = Community.delete_collection(collection)
+      assert_raise Ecto.NoResultsError, fn -> Community.get_collection!(collection.id) end
+    end
+
+    test "change_collection/1 returns a collection changeset" do
+      collection = collection_fixture()
+      assert %Ecto.Changeset{} = Community.change_collection(collection)
+    end
+  end
 end
