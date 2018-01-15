311b3c15ff5becf2376293b0899eed7f65c6391c
Vinh Nguyen <kurei@axcoto.com>
2018-01-15 13:22:10 -0800

    Fix migration

--- community/betterdev/priv/repo/migrations/20170816044500_add_bot_channel_to_user.exs
+++ community/betterdev/priv/repo/migrations/20170816044500_add_bot_channel_to_user.exs
@@ -5,8 +5,6 @@ defmodule Betterdev.Repo.Migrations.AddBotChannelToUser do
     alter table(:accounts_users) do
       add :channel, :string
       add :channel_user_id, :string
-
-      create index(:community_link_tags, [:link_id, :tag_id], unique: true)
     end
   end
 end
