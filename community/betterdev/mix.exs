701d2a6e0bc8b92e1ebe3cdde896f83833aba5ff
Vinh Nguyen <kurei@axcoto.com>
2018-02-08 00:04:07 -0800

    Update mariaex

--- community/betterdev/mix.exs
+++ community/betterdev/mix.exs
@@ -33,7 +33,7 @@ defmodule Betterdev.Mixfile do
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.2"},
      {:ecto, "~> 2.0"},
-     {:mariaex, ">= 0.0.0"},
+     {:mariaex, ">= 0.8.4"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
