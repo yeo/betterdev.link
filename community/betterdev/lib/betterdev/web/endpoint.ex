2c92d56ae0c7171957c23793596e88cb36128ae0
Vinh Nguyen <kurei@axcoto.com>
2018-02-07 23:48:20 -0800

    some fix for phoenix 1.3

--- community/betterdev/lib/betterdev/web/endpoint.ex
+++ community/betterdev/lib/betterdev/web/endpoint.ex
@@ -1,6 +1,15 @@
 defmodule Betterdev.Web.Endpoint do
   use Phoenix.Endpoint, otp_app: :betterdev
 
+	def init(_key, config) do
+		if config[:load_from_system_env] do
+			port = System.get_env("PORT") || raise "expected the PORT environment variable to be set"
+			{:ok, Keyword.put(config, :http, [:inet6, port: port])}
+		else
+			{:ok, config}
+		end
+	end
+
   socket "/socket", Betterdev.Web.UserSocket
 
   # Serve at "/" the static files from "priv/static" directory.
