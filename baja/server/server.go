95584c42992c67ed01354ea77b84e200bad78866
Vinh Nguyen <kurei@axcoto.com>
2018-04-22 02:04:12 -0700

    Log to stdout

--- baja/server/server.go
+++ baja/server/server.go
@@ -1,8 +1,6 @@
 package server
 
 import (
-	"os"
-
 	"github.com/labstack/echo"
 	"github.com/mongodb/mongo-go-driver/mongo"
 	log "github.com/sirupsen/logrus"
@@ -24,12 +22,6 @@ func Run(addr string) {
 		db:     dao.Connect(config.MongoURI),
 	}
 
-	file, err := os.OpenFile("/var/log/bd/click.log", os.O_CREATE|os.O_WRONLY, 0666)
-	if err != nil {
-		log.Fatal("Cannot open log file")
-	}
-
-	s.log.Out = file
 	router(e, s)
 	e.Logger.Fatal(e.Start(addr))
 }
